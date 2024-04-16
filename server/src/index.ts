1169b568e61b63954a2ef2f536d917b48725cba0
yk-jp <k82.y12@gmail.com>
2024-04-15 19:36:33 -0700

    Set up basic server.

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const port = 8080;
+const app = express();
+
+app.use(cors({})); // add cors settings for frontend later.
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
