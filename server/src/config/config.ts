2bc58f257f67d6e15fd31071dc1dd786a39e1eaa
yk-jp <k82.y12@gmail.com>
2024-04-16 19:17:29 -0700

    Make it easier to debug code and run docker commands.

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -16,6 +16,7 @@ export const port: number = Number(process.env.API_PORT);
 // config db connection. Add some cononection error handlings if time allows to do so.
 export const db = new Pool({
   host: process.env.POSTGRES_HOSTNAME,
+  port: Number(process.env.POSTGRES_PORT),
   user: process.env.POSTGRES_USER,
   password: Number(process.env.POSTGRES_PASSWORD),
   database: process.env.POSTGRES_DB,
