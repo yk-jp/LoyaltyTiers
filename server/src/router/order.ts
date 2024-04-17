f039ec2d5ac3a49e30afafe679bfc941e0192c3a
yk-jp <k82.y12@gmail.com>
2024-04-17 16:46:18 -0700

    Add the endpoint to list customer's order since the last year.

--- server/src/router/order.ts
+++ server/src/router/order.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
-import { saveOrder } from "../controller/order";
+import { getCustomerOrder, saveOrder } from "../controller/order";
 
 export default (router: express.Router) => {
+  router.get("/order/:customerId", getCustomerOrder);
   router.post("/order/complete", saveOrder);
 };
