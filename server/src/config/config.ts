54acb62ebde330c7d58329083fbd6f46393fafb2
yk-jp <k82.y12@gmail.com>
2024-04-16 18:38:06 -0700

    Create an API to report a complete order.

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,7 +1,22 @@
+/**
+ * put all the config together here as there's not so much export data.
+ * Default values are not considered.
+ */
+
 import * as dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config({
   path: `${__dirname}/../../.env`,
 });
 
+// config api port.
 export const port: number = Number(process.env.API_PORT);
+
+// config db connection. Add some cononection error handlings if time allows to do so.
+export const db = new Pool({
+  host: process.env.POSTGRES_HOSTNAME,
+  user: process.env.POSTGRES_USER,
+  password: Number(process.env.POSTGRES_PASSWORD),
+  database: process.env.POSTGRES_DB,
+});
--- server/src/controller/order.ts
+++ server/src/controller/order.ts
@@ -1,9 +1,27 @@
 import express from "express";
 
+import { db } from "../config/config";
+import { CompleteOrderDto } from "dtos/Order.dto";
+
 export const saveOrder = async (
-  req: express.Request,
+  req: express.Request<{}, {}, CompleteOrderDto>, // type annotate request body
   res: express.Response
 ) => {
-  console.log("test");
+  const orderDetails: CompleteOrderDto = req.body;
+  try {
+    await db.query(
+      "INSERT INTO orders (id, customer_id, total_cents, ordered_at) " +
+        "VALUES($1, $2, $3, $4)",
+      [
+        orderDetails.customerId,
+        orderDetails.customerId,
+        orderDetails.totalInCents,
+        orderDetails.date,
+      ]
+    );
+  } catch (e) {
+    res.sendStatus(500);
+  }
+
   res.sendStatus(200);
 };
