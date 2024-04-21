b134335e50765abab796ae517cf0b872a73e84e7
yk-jp <k82.y12@gmail.com>
2024-04-21 07:42:34 -0700

    Remove reusable css properties from indivisual css files and put all together in index.css.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "./index.css"; // contain all the reusable css properties
+
 import { router } from "./Routes/Routes";
 
 const root = ReactDOM.createRoot(
