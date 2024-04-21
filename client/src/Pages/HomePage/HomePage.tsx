eaea32647ad54473c9f4bc3da871a2764f0b6265
yk-jp <k82.y12@gmail.com>
2024-04-21 07:50:26 -0700

    Add loading screen to homepage.

--- client/src/Pages/HomePage/HomePage.tsx
+++ client/src/Pages/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 
 import "./HomePage.css";
 
+import Spinner from "../../Components/Spinner/Spinner";
 import { Customer } from "../../models/Customer";
 import CustomerListTable from "../../Components/Customers/CustomerListTable";
 import { getCustomerList } from "../../Apis/customer";
 
 export default function HomePage() {
+  let [isLoading, setIsLoading] = useState<boolean>(true);
   let [customers, setCustomers] = useState<Customer[]>([]);
 
   useEffect(() => {
@@ -17,6 +19,7 @@ export default function HomePage() {
           return;
         }
         setCustomers(data.customers);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching customer list:", error);
       }
@@ -26,12 +29,18 @@ export default function HomePage() {
   }, [customers]);
   return (
     <div className="justify-content-center align-items-center vh-100">
-      <div>
-        <h2 className="text-align-center">Customer List</h2>
-        <div className="max-vh-80 max-w-100">
-          <CustomerListTable customers={customers} />
+      {isLoading ? (
+        <div className="vh-80 justify-content-center align-items-center">
+          <Spinner />
         </div>
-      </div>
+      ) : (
+        <div>
+          <h2 className="text-align-center">Customer List</h2>
+          <div className="max-vh-80 max-w-100">
+            <CustomerListTable customers={customers} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
